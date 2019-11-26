cb8c17fe341cfdca5f38067b1e1cadf8529aaad4
Sy2tema <bood00142@gmail.com>
2019-11-27 05:07:20 +0900

    Update: 코드 작성 진행
    window의 이벤트를 좀 더 작성했습니다.

--- Tetris/src/app.js
+++ Tetris/src/app.js
@@ -18,8 +18,30 @@ function setTable() {
     tetris.appendChild(fragment);
 }
 
+//keyup은 버튼을 눌렀다가 뗄 때 발생하는 이벤트이다.
+//keydown은 버튼을 누를 때 발생하는 이벤트이며 keypress는 방향키를 인식하지 못한다.
 window.addEventListener('keyup', (e) => {
-    console.log(e);
+    switch (e.code) {
+        case 'Space': //한 번에 밑으로 내리기
+            break;
+        case 'ArrowUp': //블럭 회전시키기
+            break;
+        default:
+            break;
+    }
+});
+
+window.addEventListener('keydown', (e) => {
+    switch (e.code) {
+        case 'ArrowRight':
+            break;
+        case 'ArrowLeft':
+            break;
+        case 'ArrowDown':
+            break;
+        default:
+            break;
+    }
 });
 
 setTable();
