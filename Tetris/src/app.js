6c32cee2fe282e680e27c3d0382173eb26e48006
Sy2tema <bood00142@gmail.com>
2019-12-01 21:34:34 +0900

    Update: 프로젝트 진행
    블록 생성 부분 및 화면 재렌더링 함수를 작성했습니다.

--- Tetris/src/app.js
+++ Tetris/src/app.js
@@ -166,14 +166,40 @@ const blocks = [
 ];
 
 //게임에 사용될 색들의 배열
-const blockColors = ['white', 'red', 'blueViolet', 'orange', 'skyblue', 'yellowgreen', 'pink', 'khaki'];
+const blockColors = ['red', 'blueViolet', 'orange', 'skyblue', 'yellowgreen', 'pink', 'khaki'];
 
 //블록의 이동가능여부를 판단하는 플래그.
 const isActiveBlock = value => (value > 0 && value < 10);
 const isUnactiveBlock = value => (value === undefined || value >= 10);
 
+//게임 화면을 표시
 function drawWindow() {
-    
+    tetrisData.forEach((cell, i) => {
+        cell.forEach((row, j) => {
+            if (row > 0) {
+                tetris.children[i].children[j].className 
+                    = tetrisData[i][j] >= 10 
+                    ? colors[tetrisData[i][j] / 10 - 1] 
+                    : colors[tetrisData[i][j] - 1];
+            } else {
+                tetris.children[i].children[j].className = '';
+            }
+        });
+    });
+}
+
+//다음에 나올 블록을 화면에 표시
+function drawNextBlock() {
+    const nextTable = document.querySelector('#nextTable');
+    nextTable.querySelectorAll('tr').forEach((cell, i) => {
+        Array.from(cell.children).forEach((row, j) => {
+            if (nextBlock.shape[0][i] && nextBlock.shape[0][i][j] > 0) {
+                nextBlock.querySelectorAll('tr')[i].children[j].className = colors[nextBlock.numberCode - 1];
+            } else {
+                nextTable.querySelectorAll('tr')[i].children[j].className = 'white';
+            }
+        });
+    });
 }
 
 //게임 화면 생성
@@ -193,4 +219,150 @@ function init() {
     tetris.appendChild(fragment);
 }
 
+
+//블록 생성
+function createBlock() {
+    if (!currentBlock) {
+        //블록들을 정의해놓은 배열들 중에서 랜덤으로 한 종류를 뽑는다.
+        currentBlock = blocks[Math.floor(Math.random() * blocks.length)];
+    } else {
+        currentBlock = nextBlock;
+    }
+    
+    currentBlock.currentShapeIndex = 0;
+    nextBlock = blocks[Math.floor(Math.random() * blocks.length)];
+    drawNextBlock();
+
+    //처음 블록이 등장할 장소를 가상의 칸을 설정해 한칸 위로 설정하는데 이를 통해
+    //3X3 배열인 블록들이 최상단부터 시작할 수 있도록 만들어준다.
+    if (currentBlock.numberCode === 7) {
+        currentTopLeft = [-2, 3];
+    } else {
+        currentTopLeft = [-1, 3];
+    }
+
+    //게임오버 조건을 판단한다.
+    let isGameOver = false;
+    currentBlock.shape[0].slice(1).forEach((cell, i) => {
+        cell.forEach((row, j) => {
+            if (row && tetrisData[i][j + 3]) {
+                isGameOver = true;
+            }
+        });
+    });
+
+    //블록 데이터를 생성한다.
+    currentBlock.shape[9].slice(1).forEach((cell, i) => {
+        cell.forEach((row, j) => { 
+            if (row) {
+                tetrisData[i][j + 3] = currentBlock.numberCode;
+            }
+        });
+    });
+
+    if (isGameOver) {
+        clearInterval(interval);
+        drawWindow();
+        alert('게임이 종료되었습니다.');
+    } else {
+        drawWindow();
+    }
+}
+
+//가로의 한 줄이 블록으로 다 찼는지 검사
+function checkRows() {
+    const fullRows = [];
+    tetrisData.forEach((cell, i) => {
+        let count = 0;
+        cell.forEach((row, j) => {
+            if (row > 0) {
+                count++;
+            }
+        });
+
+        if (count === 10) {
+            fullRows.push(i);
+        }
+    });
+
+    const fullRowsCount = fullRows.length;
+    tetrisData = tetrisData.filter((row, i) => !fullRows.includes(i));
+
+    for (let i = 0; i < fullRowsCount; i++) {
+        tetrisData.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+
+    let score = parseInt(document.querySelector('#score').textContent, 10);
+    score += fullRowsCount ** 2;
+    document.querySelector('#score').textContent = String(score);
+}
+
+function lowerBlock() {
+    const nextTopLeft = [currentTopLeft[0] + 1, currentTopLeft[1]];
+    const activeBlocks = [];
+    let canBlockDown = true;
+    let currentBlockShape = currentBlock.shape[currentBlock.currentShapeIndex];
+
+    //아래에 블럭이 있다면 작동하는 코드
+    for (let i = currentTopLeft[0]; i < currentTopLeft[0] + currentBlockShape.length; i++) {
+        if (i < 0 || i >= 20) continue;
+        for (let j = currentTopLeft[1]; j < currentTopLeft[1] = currentBlockShape.length; j++) {
+            if (isActiveBlock(tetrisData[i][j])) {
+                activeBlocks.push([i, j]);
+
+                if (isUnactiveBlock(tetrisData[i + 1] && tetrisData[i + 1][j])) {
+                    canBlockDown = false;
+                }
+            }
+        }
+    }
+
+    if (!canBlockDown) {
+        activeBlocks.forEach((blocks) => {
+            tetrisData[blocks[0][blocks[1]]] *= 10;
+        });
+
+        checkRows();
+        createBlock();
+        return false;
+    } else if (canBlockDown) {
+        //블록을 한 줄 밑으로 내린다.
+        for (let i = tetrisData.length - 1; i >= 0; i--) {
+            const cell = tetrisData[i];
+            cell.forEach((row, j) => {
+                if (row < 10 && tetrisData[i + 1] && tetrisData[i + 1][j] < 10) {
+                    tetrisData[i + 1][j] = row;
+                    tetrisData[i][j] = 0;
+                }
+            });
+        }
+
+        currentTopLeft = nextTopLeft;
+        drawWindow();
+        return true;
+    }
+}
+
+document.querySelector('#stop').addEventListener('click', function () {
+    clearInterval(interval);
+});
+
+document.querySelector('#restart').addEventListener('click', function () {
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(lowerBlock, 2000);
+});
+
+document.querySelector('#backgroundmusic').addEventListener('click', function () {
+    if (document.querySelector('#backgroundmusic').paused) {
+        document.querySelector('#backgroundmusic').play();
+    } else {
+        document.querySelector('#backgroundmusic').pause();
+    }
+});
+
+let interval = setInterval(lowerBlock, 2000);
+
 init();
+createBlock();
